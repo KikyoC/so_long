7e458e09e054a356a4c606fb2d35de5116230b30
togauthi <togauthier23@gmail.com>
2024-12-02 10:59:40 +0100

    C pour test

--- common/map_manager/checker/map-check.c
+++ common/map_manager/checker/map-check.c
@@ -6,7 +6,7 @@
 /*   By: togauthi <togauthi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:23:17 by togauthi          #+#    #+#             */
-/*   Updated: 2024/11/28 13:22:00 by togauthi         ###   ########.fr       */
+/*   Updated: 2024/12/02 10:59:18 by togauthi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,8 @@ int	good_name(char *name)
 */
 char	*check(t_map *map, t_player *player, char *name)
 {
+	if (!good_name(name))
+		return ("Map is not good format. Check if file is a \".ber\" file");
 	if (!is_rect(map))
 		return ("Map is not rectangular");
 	if (!check_border(map))
@@ -43,7 +45,5 @@ char	*check(t_map *map, t_player *player, char *name)
 			"1 Exit Position and at least 1 Collectible Position");
 	if (!is_solvable(map, player))
 		return ("There is no way to solve the map. Check your map");
-	if (!good_name(name))
-		return ("Map is not good format. Check if file is a \".ber\" file");
 	return (NULL);
 }
